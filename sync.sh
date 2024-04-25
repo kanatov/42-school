52038cdbe2809accedea3345d3c7c98619f862ba
kanatov <ilya.kanatov@gmail.com>
2024-04-25 05:42:02 +0100

    Delete sync.sh

--- sync.sh
+++
@@ -1,7 +0,0 @@
-clear
-rm -r ./git/libft/
-~/Documents/libft/make fclean
-rsync -ru --progress --exclude=.git ~/Documents/libft/ ~/Documents/git/libft/
-git add *
-git commit -m "sync update"
-git push
